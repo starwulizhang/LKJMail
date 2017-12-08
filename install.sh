d99a2e4e1dbaa715a43bd95d90ae0328da9109d6
污污的大大张 <34328108+starwulizhang@users.noreply.github.com>
2017-12-08 22:38:38 +0800

    正式版1.0

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 echo "Haproxy正在安装"
 yum -y install haproxy wget
 echo "下载Haproxy配置文件中"
+rm -rf /etc/haproxy/haproxy.cfg
 wget https://raw.githubusercontent.com/starwulizhang/haproxy-mail/master/haproxy.cfg -O /etc/haproxy/haproxy.cfg
-echo "添加Haproxy到开机自启中"
+echo "启动Haproxy并添加到开机自启中"
 service haproxy start
 chkconfig --level 2345 haproxy on
+echo "已添加到开机自启中"
+echo "安装完毕，感谢使用-乐开景云计算（yun.lekaijing.com）"
